2bcfc11511464b6c4960d6bce1e890adedf9ec5a
Arnab-g9 <arnabghorai15@gmail.com>
2026-07-14 00:56:40 +0530

    updated code

--- backend/04_post_app/src/app.js
+++ backend/04_post_app/src/app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const multer = require('multer');
 const uploadFile = require('./services/storage.service');
+const postModal = require("./models/post.model")
+
 const app = express();
 
 
@@ -14,6 +16,24 @@ app.post('/create-post', upload.single("image"), async (req, res)=>{
     const result = await uploadFile(req.file.buffer);
 
     console.log("result >>>",result);
+
+    const post = await postModal.create({
+        image: result.url,
+        caption: req.body.caption
+    })
+
+    return res.status(201).json({
+        message: "Post created successfully",
+        post
+    })
+})
+
+app.get("/posts", async(req, res)=>{
+    const posts = await postModal.find();
+    return res.status(200).json({
+        message: "Post fetched successfully",
+        posts
+    })
 })
 
 module.exports = app;
